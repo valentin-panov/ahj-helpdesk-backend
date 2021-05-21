9823b05edb3d133ceeceda16faf43e8a9c651abf
Valentin Panov <valentin.panov@me.com>
2021-05-22 00:19:01 +0300

    localhost works

--- idGenerator.js
+++ idGenerator.js
@@ -2,4 +2,4 @@ function generateId() {
   return `${Date.now()}-${Math.random()}`;
 }
 
-exports.generateId = generateId;
+module.exports = generateId;
--- serverEngine.js
+++ serverEngine.js
@@ -1,26 +1,26 @@
 const Koa = require('koa');
 const cors = require('@koa/cors');
 const koaBody = require('koa-body');
-const idGenerator = require('./idGenerator');
+const generateId = require('./idGenerator');
 const formatDate = require('./formatDate');
 
 const tickets = [
   {
-    id: idGenerator.generateId(),
+    id: generateId(),
     name: 'Поменять краску в принтере, ком. 404',
     description: 'Принтер HP LJ 1210, картриджи на складе',
     status: false,
     created: formatDate(new Date()),
   },
   {
-    id: idGenerator.generateId(),
+    id: generateId(),
     name: 'Переустановить Windows, ПК-Hall24',
     description: 'Развернуть образ и закрыть все админские права',
     status: false,
     created: formatDate(new Date()),
   },
   {
-    id: idGenerator.generateId(),
+    id: generateId(),
     name: 'Установить обновление KB-XXX',
     description:
       'Вышло критическое обновление Windows, нужно поставить обновления в следующем приоритете:<br>1. Сервера (не забыть сделать бэкап!)<br>2. Рабочие станции',
@@ -40,13 +40,10 @@ serverEngine.use(async (ctx) => {
     case 'allTickets':
       ctx.response.body = tickets;
       return;
-    case 'ticketById':
-      ctx.response.body = tickets.find((ticket) => ticket.id === id);
-      return;
     case 'createTicket':
-      const newTicketData = JSON.parse(ctx.request.body);
+      const newTicketData = ctx.request.body;
       const newTicket = {
-        id: idGenerator.generateId(),
+        id: generateId(),
         name: newTicketData.name,
         status: false,
         description: newTicketData.description || '',
@@ -62,14 +59,16 @@ serverEngine.use(async (ctx) => {
       return;
     case 'updateById':
       const updIndex = tickets.findIndex((ticket) => ticket.id === id);
-      const updTicketData = JSON.parse(ctx.request.body);
+      const updTicketData = ctx.request.body;
       const ticket = {
         ...tickets[updIndex],
         ...updTicketData,
       };
-      console.log(ticket);
       tickets.splice(updIndex, 1, ticket);
       ctx.response.body = tickets;
+      return;
+    case 'ticketById':
+      ctx.response.body = tickets.find((ticket) => ticket.id === id);
       return;
     default:
       ctx.response.status = 404;
